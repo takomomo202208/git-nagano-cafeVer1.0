79cdc6d8f0561969cf64b7afa99d971c4f4f0f72
YK2022-July <105860045+YK2022-July@users.noreply.github.com>
2022-09-19 08:17:10 +0000

    [Add]order/order_detail/cart_itemのモデルファイルとマイグレーションファイルを編集

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,2 +1,4 @@
 class CartItem < ApplicationRecord
+  belongs_to :customers, dependent: :destroy
+  belongs_to :items, dependent: :destroy
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,4 @@
 class Order < ApplicationRecord
+  belongs_to :customers, dependent: :destroy
+  has_many :order_details, dependent: :destroy
 end
