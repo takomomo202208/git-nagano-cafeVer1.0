b5dcedd6ecd1a068b563dad96c4d473094212313
YK2022-July <105860045+YK2022-July@users.noreply.github.com>
2022-09-21 05:17:07 +0000

    [Add]商品一覧画面

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,6 @@ class Item < ApplicationRecord
   has_many :order_details, dependent: :destroy
   has_many :cart_items, dependent: :destroy
   belongs_to :genres
+  has_one_attached :image
 end
+cd
--- db/schema.rb
+++ db/schema.rb
@@ -40,34 +40,6 @@ ActiveRecord::Schema.define(version: 2022_09_19_085016) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
-  create_table "active_storage_attachments", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "record_type", null: false
-    t.bigint "record_id", null: false
-    t.bigint "blob_id", null: false
-    t.datetime "created_at", null: false
-    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
-    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
-  end
-
-  create_table "active_storage_blobs", force: :cascade do |t|
-    t.string "key", null: false
-    t.string "filename", null: false
-    t.string "content_type"
-    t.text "metadata"
-    t.string "service_name", null: false
-    t.bigint "byte_size", null: false
-    t.string "checksum", null: false
-    t.datetime "created_at", null: false
-    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
-  end
-
-  create_table "active_storage_variant_records", force: :cascade do |t|
-    t.bigint "blob_id", null: false
-    t.string "variation_digest", null: false
-    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
-  end
-
   create_table "addresses", force: :cascade do |t|
     t.integer "customer_id", null: false
     t.string "name", null: false
@@ -100,13 +72,13 @@ ActiveRecord::Schema.define(version: 2022_09_19_085016) do
     t.index ["item_id"], name: "index_cart_items_on_item_id"
   end
 
-  create_table "customers", primary_key: "customer_id", force: :cascade do |t|
+  create_table "customers", id: false, force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "last_name", null: false
     t.string "first_name", null: false
-    t.string "last_kana", null: false
-    t.string "first_kana", null: false
+    t.string "last_name_kana", null: false
+    t.string "first_name_kana", null: false
     t.string "postal_code", null: false
     t.string "address", null: false
     t.string "telephone_number", null: false
@@ -116,8 +88,6 @@ ActiveRecord::Schema.define(version: 2022_09_19_085016) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["customer_id", "last_kana", "first_kana"], name: "index_customers_on_customer_id_and_last_kana_and_first_kana", unique: true
-    t.index ["customer_id", "last_name", "first_name"], name: "index_customers_on_customer_id_and_last_name_and_first_name", unique: true
     t.index ["email"], name: "index_customers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
   end
@@ -129,11 +99,14 @@ ActiveRecord::Schema.define(version: 2022_09_19_085016) do
   end
 
   create_table "items", force: :cascade do |t|
+    t.integer "genre_id", null: false
     t.string "name", null: false
     t.text "introduction", null: false
     t.integer "price", null: false
+    t.boolean "is_active", default: true, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["genre_id"], name: "index_items_on_genre_id"
   end
 
   create_table "order_details", force: :cascade do |t|
@@ -141,6 +114,7 @@ ActiveRecord::Schema.define(version: 2022_09_19_085016) do
     t.integer "order_id", null: false
     t.integer "amount", null: false
     t.integer "price", null: false
+    t.integer "making_status", default: 0, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["item_id"], name: "index_order_details_on_item_id"
@@ -155,6 +129,7 @@ ActiveRecord::Schema.define(version: 2022_09_19_085016) do
     t.integer "payment_method", default: 0, null: false
     t.integer "total_payment", null: false
     t.integer "shipping_cost", null: false
+    t.integer "status", default: 0, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["customer_id"], name: "index_orders_on_customer_id"
@@ -162,11 +137,10 @@ ActiveRecord::Schema.define(version: 2022_09_19_085016) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
-
   add_foreign_key "addresses", "customers"
-
   add_foreign_key "cart_items", "customers"
   add_foreign_key "cart_items", "items"
+  add_foreign_key "items", "genres"
   add_foreign_key "order_details", "items"
   add_foreign_key "order_details", "orders"
   add_foreign_key "orders", "customers"
