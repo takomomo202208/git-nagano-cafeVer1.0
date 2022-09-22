05576b06471bae6e9ee208762fa9bfd4eadfd67e
YK2022-July <105860045+YK2022-July@users.noreply.github.com>
2022-09-22 15:17:30 +0000

    [Update]商品一覧画面(0922)

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,12 +1,12 @@
 class Item < ApplicationRecord
   has_many :order_details, dependent: :destroy
   has_many :cart_items, dependent: :destroy
-  belongs_to :genres
+  belongs_to :genres, optional: true
   has_one_attached :image
 
   def get_image
     unless image.attached?
-      file_path = Rails.root.join('app/assets/no_image.jpg')
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
       image.attache(io:File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
     image
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,89 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Genre.create!(
+  [
+    {
+      name: 'ケーキ',
+    },
+    {
+      name: '焼き菓子',
+    },
+    {
+      name: 'プリン',
+    },
+    {
+      name: 'キャンディ',
+    }
+  ]
+)
+
+Item.create!(
+  [
+    {
+      genre_id: 1,
+      name: 'チーズケーキ',
+      introduction: '新商品！',
+      price: 500,
+      is_active: true,
+      #image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/app/assets/images"), filename:"no_image.jpg")
+    },
+    {
+      genre_id: 2,
+      name: 'マカロン',
+      introduction: '新商品！',
+      price: 300,
+      is_active: true,
+      #image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/app/assets/images"), filename:"no_image.jpg")
+    },
+    {
+      genre_id: 3,
+      name: 'なめらかプリン',
+      introduction: '新商品！',
+      price: 400,
+      is_active: true,
+      #image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/app/assets/images"), filename:"no_image.jpg")
+    },
+    {
+      genre_id: 4,
+      name: '生キャラメル',
+      introduction: '新商品！',
+      price: 200,
+      is_active: true,
+      #image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/app/assets/images"), filename:"no_image.jpg")
+    },
+    {
+      genre_id: 1,
+      name: 'チェリータルト',
+      introduction: '新商品！',
+      price: 500,
+      is_active: true,
+      #image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/app/assets/images"), filename:"no_image.jpg")
+    },
+    {
+      genre_id: 2,
+      name: 'クッキーアソート',
+      introduction: '新商品！',
+      price: 1000,
+      is_active: true,
+      #image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/app/assets/images"), filename:"no_image.jpg")
+    },
+    {
+      genre_id: 3,
+      name: 'かぼちゃプリン',
+      introduction: '新商品！',
+      price: 400,
+      is_active: true,
+      #image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/app/assets/images"), filename:"no_image.jpg")
+    },
+    {
+      genre_id: 4,
+      name: '千歳飴',
+      introduction: '新商品！',
+      price: 100,
+      is_active: true,
+      #image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/app/assets/images"), filename:"no_image.jpg")
+    }
+  ]
+)
