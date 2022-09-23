ebd341dd0fa7de35a7a1911ac18249dbcfac0e38
tsuyoron <108647290+tsuyoron@users.noreply.github.com>
2022-09-23 14:07:05 +0000

    [Add]注文機能作成

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -2,9 +2,11 @@ class Public::OrdersController < ApplicationController
   #sbefore_action :authenticate_customer!
 
   def new
+    @order = Order.new
   end
 
   def create
+
   end
 
   def show
@@ -14,9 +16,32 @@ class Public::OrdersController < ApplicationController
   end
 
   def confirm
+    @order = Order.new(order_params)
+    @cart_items = current_customer.cart_items.all
+    pp current_customer.cart_items.all
+    # addressにresidenceの値がはいっていれば
+    if params[:order][:addresses] == "residence"
+      @order.postal_code = current_customer.postal_code
+      @order.address     = current_customer.address
+      @order.name        = current_customer.last_name + current_customer.first_name
+    end
+
   end
 
   def complete
+    # addressにresidenceの値がはいっていれば
+      @order.postal_code = current_customer.postal_code
+      @order.address     = current_customer.residence
+      @order.name        = current_customer.last_name + current_customer.first_name
+
   end
 
+private
+def order_params
+  params.require(:order).permit(:payment_method,:address)
+end
+
+  def address_params
+    params.require(:order).permit(:postal_code, :address, :name)
+  end
 end
