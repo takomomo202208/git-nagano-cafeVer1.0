17864f4c7510731a5f6dcd76aacfc20fe844d95b
TatsuyaYasui <109195719+aboutyasui@users.noreply.github.com>
2022-09-23 08:57:32 +0000

    [Add]jpの記述追加#2

--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -1,6 +1,6 @@
 class OrderDetail < ApplicationRecord
-  belongs_to :items
-  belongs_to :orders
+  belongs_to :item
+  belongs_to :order
 
   enum making_status: { "Cannot be manufactured": 0, "Waiting for production": 1 , "In production":2 , "Completion of production":3 }
   #config/locaales/ja.ymlにて日本語してます
