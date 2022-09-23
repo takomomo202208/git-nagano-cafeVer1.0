b449a00363de1817a6f2c3e7cbf3f69972e3709b
tsuyoron <108647290+tsuyoron@users.noreply.github.com>
2022-09-23 10:40:39 +0900

    [Add]full_name/full_name_kanaの修正

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -7,5 +7,12 @@ class Customer < ApplicationRecord
   has_many :cart_items, dependent: :destroy
   has_many :addresses,dependent: :destroy
 
+  def full_name #苗字と名前をくっつけるメソッド
+    self.last_name + " " + self.first_name
+  end
 
+  def full_name_kana #カナ妙とカナ名をくっつけるメソッド
+    self.last_name_kana + " " + self.first_name_kana
+  end
+  
 end
