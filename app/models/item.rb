fff1ef5199786e7c06fa74cf6859c39b2fd5a661
YK2022-July <105860045+YK2022-July@users.noreply.github.com>
2022-09-23 08:16:36 +0000

    [Update]商品一覧画面

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,11 +5,20 @@ class Item < ApplicationRecord
   belongs_to :genre, optional: true
   has_one_attached :image
 
+  #def get_image
+    #unless image.attached?
+      #file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      #image.attache(io:File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    #end
+    #image.variant(resize_to_limit: [width, height]).processed
+  #end
+
   def get_image
-    unless image.attached?
-      file_path = Rails.root.join('app/assets/images/no_image.jpg')
-      image.attache(io:File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    if image.attached?
+      image
+    else
+      'no_image.jpg'
     end
-    image
   end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,37 +7,6 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-  20.times do |n|
-    Customer.create!(
-      email: "test#{n + 1}@test.com",
-      last_name: "太郎#{n + 1}",
-      first_name: "てすと#{n + 1}",
-      last_name_kana: "タロウ#{n + 1}",
-      first_name_kana: "テスト#{n + 1}",
-      postal_code: "111#{n + 1}",
-      password: "aaa111#{n + 1}",
-      address: "日本#{n + 1}",
-      telephone_number: "0120000#{n + 1}",
-      is_deleted: false,
-    )
-  end
-
-Admin.create!(
-   email: 'bonboncho!@test.com',
-   password: 'momotako',
-)
-
-
-  20.times do |n|
-    Item.create!(
-      genre_id: 1,
-      name: "BonBonケーキ#{n + 1}",
-      introduction: "真心こめて作りました💛#{n + 1}",
-      price: "893",
-      is_active: false,
-    )
-  end
-  
 Genre.create!(
   [
     {
