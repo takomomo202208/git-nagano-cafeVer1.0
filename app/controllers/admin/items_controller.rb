1e25b80f3b3a0521122e43fe91e328c3d0816e5d
SayakaIw <160105syk@gmail.com>
2022-09-23 08:18:17 +0000

    [Add]adminItemsNew

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -2,12 +2,21 @@ class Admin::ItemsController < ApplicationController
   #before_action :authenticate_user!
 
   def new
+    @item = Item.new
   end
 
   def create
+    # @item = Item.new(genre_id: params[:item][:genre_id] )ジャンルのプルダウン
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to item_path(@item.id)
+    else
+      render :new
+    end
   end
 
   def index
+    @item = Item.page(params[:page])
   end
 
   def show
