bb15055728f9a414caab492c9a82e7f60fa01728
tsuyoron <108647290+tsuyoron@users.noreply.github.com>
2022-09-24 16:41:45 +0900

    [Fix]画像表示について修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -16,22 +16,6 @@ class Item < ApplicationRecord
     image.variant(resize_to_limit: [width, height]).processed
   end
 
-  #def get_image
-    #unless image.attached?
-      #file_path = Rails.root.join('app/assets/images/no_image.jpg')
-      #image.attache(io:File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
-    #end
-    #image.variant(resize_to_limit: [width, height]).processed
-  #end
-
-  # def get_image
-  #   if image.attached?
-  #     image
-  #   else
-  #     'no_image.jpg'
-  #   end
-  # end
-
   def taxin_price
     price*1.1
   end
