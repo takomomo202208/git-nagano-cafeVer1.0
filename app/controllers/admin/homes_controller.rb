b5ae6fe1dec3fe2161d245c8a6f87e82f0aadb82
TatsuyaYasui <109195719+aboutyasui@users.noreply.github.com>
2022-09-25 06:10:13 +0000

    [add]topページ商品並び替え処理追加

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,11 +1,8 @@
 class Admin::HomesController < ApplicationController
 
   def top
-    # @order = Order.find(params[:id])
-    # @orders = Order.all
-    #@orders = Order.page(params[:page])
     redirect_to admin_orders_path
   end
 
-  
+
 end
